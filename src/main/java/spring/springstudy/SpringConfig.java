c7db72cf3dfd6c8cde0faf6873d7e944be3398f4
guswl197 <guswl197@ajou.ac.kr>
2022-03-15 22:34:56 +0900

    aop 추가

---
+++ src/main/java/spring/springstudy/SpringConfig.java
@@ -0,0 +1,30 @@
+package spring.springstudy;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import spring.springstudy.aop.TimeTraceAop;
+import spring.springstudy.repository.*;
+import spring.springstudy.service.MemberService;
+
+import javax.persistence.EntityManager;
+import javax.sql.DataSource;
+
+@Configuration
+public class SpringConfig {
+
+    private final MemberRepository memberRepository;
+
+    @Autowired
+    public  SpringConfig(MemberRepository memberRepository){
+        this.memberRepository=memberRepository;
+    }
+
+    @Bean
+    public MemberService memberService(){
+        return new MemberService(memberRepository);
+    }
+
+
+
+}
---
+++ src/main/java/spring/springstudy/service/MemberService.java
@@ -0,0 +1,43 @@
+package spring.springstudy.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import spring.springstudy.domain.Member;
+import spring.springstudy.repository.MemberRepository;
+import spring.springstudy.repository.MemoryMemberRepository;
+
+import javax.transaction.Transactional;
+import java.sql.SQLException;
+import java.util.List;
+import java.util.Optional;
+
+@Transactional
+public class MemberService {
+    private final MemberRepository memberRepository;
+
+    public MemberService(MemberRepository memberRepository)  {
+        this.memberRepository= memberRepository;
+    }
+
+    public Long join(Member member){
+        validateDuplicateMember(member);
+        memberRepository.save(member);
+        return member.getId();
+    }
+
+    private void validateDuplicateMember(Member member){
+        memberRepository.findByName(member.getName())
+                .ifPresent(m->{
+                    throw new IllegalStateException("이미 존재하는 회원입니다.");
+                });
+    }
+
+    public List<Member> findMembers(){
+        return memberRepository.findAll();
+    }
+
+    public Optional<Member> findOne(Long memberId){
+        return memberRepository.findById(memberId);
+    }
+
+}
